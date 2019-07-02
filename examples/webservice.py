9b3fa1c102c669accf8a971a1aeef7e23877a0d8
Alisher A. Khassanov <a.khssnv@gmail.com>
2019-07-02 19:35:45 +0400

    HTTPS support

--- examples/webservice.py
+++ examples/webservice.py
@@ -6,6 +6,7 @@ if not (version_info[0] >= 3 and version_info[1] >= 7):
     raise Exception('python3.7 or higher required')
 
 
+import ssl
 import asyncio
 from aiohttp import web
 import nest_asyncio
@@ -47,6 +48,10 @@ async def handle(req):
 if __name__ == "__main__":
     args = parser.parse_args()
     nest_asyncio.apply()
+
+    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_context.load_cert_chain('cert.crt', 'cert.key')
+
     app = web.Application()
     app.add_routes([web.get("/peregrine", handle)])
-    web.run_app(app, port=args.port)
+    web.run_app(app, port=args.port, ssl_context=ssl_context)
